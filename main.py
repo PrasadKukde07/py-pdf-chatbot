b311886ebb9b1e10f3a73dac8cfa85cea8c08808
Prasad Kukde <124510084+PrasadKukde07@users.noreply.github.com>
2025-12-16 15:09:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import streamlit as st
 from PyPDF2 import PdfReader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 import os
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_google_genai import ChatGoogleGenerativeAI
 import google.generativeai as genai
-from langchain.vectorstores import FAISS
-from langchain.chains.question_answering import load_qa_chain
-from langchain.prompts import PromptTemplate
+from langchain_community.vectorstores import FAISS
+from langchain_classic.chains.question_answering import load_qa_chain
+from langchain_core.prompts import PromptTemplate
 
 
 def get_pdf_text(pdf_docs):
@@ -152,4 +152,5 @@ def main():
 
 
 if __name__ == "__main__":
+
     main()
